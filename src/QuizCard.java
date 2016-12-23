1a12f1c5d82983e54e93dcefefd8f75a4337c8ea
Roman Pryshchepa <roma.kiev.ua@gmail.com>
2016-12-23 16:59:28 +0200

    commit work 23122016 p526

---
+++ src/QuizCard.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+/**
+ * Created by pm-01 on 22.12.2016.
+ */
+public class QuizCard {
+    private String question;
+    private String answer;
+
+
+    public QuizCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return this.question;
+    }
+
+    public String getAnswer() {
+        return this.answer;
+    }
+}
