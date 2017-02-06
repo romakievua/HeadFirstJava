b8a143134bb0eace986371e21582c459adaec2ee
roman <roman>
2017-02-06 21:23:47 +0200

    Recursion 2017-02-06

--- src/Map1.java
+++ src/Map1.java
@@ -13,39 +13,55 @@ public class Map1 {
         */
 
         //Map<String, String> map = new HashMap<String, String>();
-        Map<String, Integer> map = new HashMap<String, Integer>();
+        //Map<String, Integer> map = new HashMap<String, Integer>();
+        Map<String, Boolean> map = new HashMap<String, Boolean>();
 
         //"salad": "oil", "potato": "ketchup"
         //map.put("salad", "oil");
         //map.put("potato", "ketchup");
         //map.put("c", "meh");
 
-        String[] strArray = new String[]{"aa", "bb", "cc", "aAA", "cCC", "d"};
+        String[] strArray = new String[]{"ax", "bx", "ay", "by", "ai", "aj", "bx", "by"};
+        String[] strArrayOut = new String[strArray.length];
 
         Map1 map1 = new Map1();
-        map = map1.word0(strArray);
-
-        for (Map.Entry<String, String> pair : map.entrySet()) {
+        strArrayOut = map1.allSwap(strArray);
+        //System.out.println(result);
+        String div = "";
+        for (int i = 0; i < strArrayOut.length; i++) {
+            System.out.print(div + strArrayOut[i]);
+            div = ", ";
+        }
+        System.out.println();
+/*
+        for (Map.Entry<String, Boolean> pair : map.entrySet()) {
             String key = pair.getKey();                      //ключ
-            String value = pair.getValue();                  //значение
+            Boolean value = pair.getValue();                  //значение
             System.out.println(key + ":" + value);
         }
+*/
     }
 
-    public Map<String, String> word0(String[] strings) {
-        Map<String, String> map = new HashMap<String, String>();
-        for (String string : strings) {
-            String str = new String(string.substring(0,1));
-            if (map.containsKey(str)) {
-                map.put(str, map.get(str)+string);
+    public String[] allSwap(String[] strings) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < strings.length; i++) {
+            String firstChar = new String(strings[i].substring(0, 1));
+            if (map.containsKey(firstChar)) {
+                if (map.get(firstChar) != -1) {
+                    String helpStr = strings[i];
+                    strings[i] = strings[map.get(firstChar)];
+                    strings[map.get(firstChar)] = helpStr;
+                    map.put(firstChar, -1);
+                }
             }
             else {
-                map.put(str, string);
+                map.put(firstChar, i);
             }
         }
-        return map;
-    }
 
+        return strings;
+    }
+/*
     public Map<String, String> mapBully(Map<String, String> map) {
         if (map.containsKey("potato")) {
             String value = map.get("potato");
@@ -59,5 +75,5 @@ public class Map1 {
 
         return map;
     }
-
+*/
 }
